df4efa687b7fe4cd141bb7714c0db7da6c9ce264
Kamal Garg <kamalgarg6626@gmail.com>
2025-08-28 13:11:04 +0530

    cors changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,26 @@ connectCloudinary();
 //middlewares
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  "https://patient-doctor-scheduling-platform-tau.vercel.app",
+  "https://patient-doctor-scheduling-platform-liart.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5174", // vite local dev
+  "http://localhost:3000"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 //api endpoints
 
